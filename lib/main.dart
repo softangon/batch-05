89b0f27c232a36a8ee53427200d6ff4349a34043
SaifullaJubair <saifullajubair6@gmail.com>
2026-06-29 23:08:29 +0600

    stack

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(
     MaterialApp(
       //debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Padding(
-            padding: EdgeInsets.symmetric(vertical: 25, horizontal: 10),
-          child: Container(
-            padding: EdgeInsets.all(5),
-            height: 130,
-            width: double.infinity,
-            color: Colors.red.shade100,
-            child: Row(
+        body: Column(
+          children: [
+            Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.bottomCenter,
               children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
+                Image.network(
+                    fit: BoxFit.fill,
                     height: 150,
-                    width: 100,
-                    color: Colors.white,
-                  ),
+                    width: double.infinity,
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDrSGOQ27yddVo1D9CfqYbTaH-wUaMKTZIeA&s"),
+                Positioned(
+                    bottom: -45,
+                    child:  CircleAvatar(
+                      radius: 60,
+                      backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDTpIv6LZiFtm_tWGTC_U4W8DKBBRql7hqpQ&s"),
+                    )
                 ),
-                SizedBox(width: 15,),
-                Expanded(
-                  flex: 3,
-                  child: Column(
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Classic Polo T-Shirt for Men",style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold),),
-                      Text("Brand : Easy"),
-                      Row(
-                        children: [
-                          Icon(Icons.star,color: Colors.orange,),
-                          Icon(Icons.star,color: Colors.orange,),
-                          Icon(Icons.star,color: Colors.orange,),
-                          Icon(Icons.star,color: Colors.orange,),
-                          Icon(Icons.star),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text("\$25",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),),
-                          SizedBox(width: 10,),
-                          Text("\$30",
-                            style: TextStyle(
-                                fontSize: 17,
-                                decoration: TextDecoration.lineThrough),),
-                        ],
-                      ),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Container(
-                              height: 30,
-                              width: 50,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  border: Border.all(
-                                      color: Colors.black,width: 2)
-                              ),
-                              child: Icon(Icons.add),
-                            ),
-                          ],
-                        ),
-                    ],
-                  ),
-                )
               ],
             ),
-          ),
+            SizedBox(height: 50,),
+            Text("Mamun Islam MIM",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)
+          ],
         ),
       ),
-    )
+    ),
   );
 }
